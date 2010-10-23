8ab2feadcaff723aabecc93c4ba76d9a5546cd0d
Marcio Giaxa Marinheiro <marciogm@MacBook-Pro-de-Marcio-Giaxa-Marinheiro.local>
2010-10-23 17:05:08 -0200

    initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubycast::Application.config.secret_token = '7f2ee50cede8f229b94fc3ef0873adae8e2a56979b5ac0a8e3d27006f2af08d50675b4dc97a5dd4252506b41ab8c252fa24adce6bea98c4f314596094b56d05a'
