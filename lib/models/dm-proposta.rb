9af055bcbb15feb150bf4d37b4874204505019b4
Emerson Vinicius <talk@evinicius.com>
2010-10-26 17:01:26 -0200

    Add Model Proposta

---
+++ lib/models/dm-proposta.rb
@@ -0,0 +1,14 @@
+class DmProposta
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String
+  property :email, String
+  property :url, String
+  property :description, Text
+  property :sugest, Text
+  
+  attr_accessor :id, :name, :email, :url, :description
+
+  validates_presence_of :url, :email, :name
+end
