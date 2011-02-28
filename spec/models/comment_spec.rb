9cf2d768786765f7876c1c6822a85064073631b8
marciogm <xhwoax@hotmail.com>
2011-02-28 09:24:23 -0300

    some specs for comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Comment do
-  
+  let(:comment) { Comment.new }
+
+  context "on validations" do
+    before { comment.valid? }
+
+    it "should require a author" do
+      comment.errors.on(:author).should_not be_nil      
+    end
+
+    it "should require a body" do
+      comment.errors.on(:body).should_not be_nil
+    end
+
+    it "should require a episode id" do
+      comment.errors.on(:episode_id).should_not be_nil
+    end
+  end
+
 end
