bbcf6c9f3323324e90a77e8b58c3c4dd8f79623b
marciogm <xhwoax@hotmail.com>
2011-02-28 09:21:40 -0300

    added property 'Author' to Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment
   belongs_to :episode
   
   property :id, Serial
+  property :author, String, :length => 50, :required => true
   property :body, String, :length => 255, :required => true
   property :episode_id, Integer, :required => true
 end
